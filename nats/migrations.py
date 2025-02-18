f9bcba6731bc6183bc66f0ee67b541c8f810656a
dmitriy <dmitriydmgora@gmail.com>
2025-02-18 03:20:28 +0300

    Добавил миграции nats и докер файлы для них

---
+++ nats/migrations.py
@@ -0,0 +1,20 @@
+import asyncio
+import os
+
+import nats
+import structlog
+from nats.js.api import KeyValueConfig
+
+
+async def main():
+    logger = structlog.get_logger(__name__)
+    nc = await nats.connect(os.getenv('NATS_URL'))
+    js = nc.jetstream()
+
+    logger.debug('NATS соединение установлено')
+
+    await js.create_key_value(KeyValueConfig('fsm_data_aiogram'))
+    await js.create_key_value(KeyValueConfig('fsm_states_aiogram'))
+
+if __name__ == '__main__':
+    asyncio.run(main())
